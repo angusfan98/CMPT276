f240f77c6cbdf01ab2a684753e5e5cba5e57853b
Freeman L <freemanxjl@gmail.com>
2019-06-28 18:10:14 -0700

    Test page added
    Used to test routing and navbar

--- share276/config/routes.rb
+++ share276/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
 	root 'welcome#index', as: 'home'
+	get 'test' => 'welcome#testpage'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
