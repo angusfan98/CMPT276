36595bc4bfbb75f5fadd481c96809bf93312aaf7
Artur Eginyan <artur.eginyan@gmail.com>
2019-07-02 14:03:40 -0700

    added edit and remove

--- share276/app/controllers/links_controller.rb
+++ share276/app/controllers/links_controller.rb
@@ -2,15 +2,19 @@ class LinksController < ApplicationController
     def index
         @links = Link.all
     end
-    
-    def new
-        @link = Link.new
-    end
 
     def show
         @link = Link.find(params[:id])
     end
+        
+    def new
+        @link = Link.new
+    end
      
+    def edit
+        @link = Link.find(params[:id])
+    end
+
     def create
         @link = Link.new(link_params)
        
@@ -21,6 +25,23 @@ class LinksController < ApplicationController
         end
     end
        
+    def update
+        @link = Link.find(params[:id])
+
+        if @link.update(link_params)
+            redirect_to @link
+        else
+            redner 'edit'
+        end
+    end
+
+    def destroy
+        @link = Link.find(params[:id])
+        @link.destroy
+
+        redirect_to links_path
+    end
+
     private
         def link_params
           params.require(:link).permit(:URL, :Description)
