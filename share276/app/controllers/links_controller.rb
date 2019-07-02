aaf3d9183724d27854cb0e149e6f5ec81daaeeb0
Artur Eginyan <artur.eginyan@gmail.com>
2019-07-01 19:26:14 -0700

    insert commit

---
+++ share276/app/controllers/links_controller.rb
@@ -0,0 +1,25 @@
+class LinksController < ApplicationController
+    def index
+        @links = Link.all
+    end
+
+    
+    def new
+    end
+
+    def show
+        @link = Link.find(params[:id])
+    end
+     
+    def create
+        @link = Link.new(link_params)
+       
+        @link.save
+        redirect_to @link
+    end
+       
+    private
+        def link_params
+          params.require(:link).permit(:URL, :Description)
+        end 
+end
