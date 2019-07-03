f867ca7a44dca8aa2fc864d5277b15ff65e5f283
jasdeepgill <jasdeep360@gmail.com>
2019-07-02 17:40:47 -0700

    Added validation so that each url must start with http:// or https://

--- share276/app/models/link.rb
+++ share276/app/models/link.rb
@@ -1,3 +1,5 @@
 class Link < ApplicationRecord
-    validates :URL, presence: true
+    validates :URL, presence: true ,
+    length: { minimum: 4 },format: { with: /[h]+[t]+[t]+[p]+[s]*[:]+[\/]+[\/]+./,
+    message: "must start with 'http://' or 'https://' E.g. 'https://github.com'", } 
 end
